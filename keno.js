6891ebce7cb8f2cdb23e953eea1b8e839263626c
Chris Lopez <chrislopez28@gmail.com>
2020-08-18 17:54:23 -0700

    Initial commit

---
+++ keno.js
@@ -0,0 +1,170 @@
+let svgns = "http://www.w3.org/2000/svg";
+let viewBoxWidth = 100;
+let viewBoxHeight = 80;
+let rows = 8;
+let cols = 10;
+let strokeWidth = 2.5 / rows;
+let spacing = 5;
+let rounding = 0.25;
+
+gridX = viewBoxWidth/cols;
+gridY = viewBoxHeight/rows;
+
+var currentNum = 1;
+
+for (let i = 0; i < rows; i++) {
+	for (let j = 0; j < cols; j++) {
+  	let g = document.createElementNS(svgns, 'g');
+    g.setAttributeNS(null, 'font-size', 4);
+ 		g.setAttributeNS(null, 'id', `${i}${j}`);
+    
+  	let rect = document.createElementNS(svgns, 'rect');
+    rect.setAttributeNS(null, 'x', j * gridX + spacing * strokeWidth);
+    rect.setAttributeNS(null, 'y', i * gridY + spacing * strokeWidth);
+    rect.setAttributeNS(null, 'height', gridY - spacing * strokeWidth);
+    rect.setAttributeNS(null, 'width', gridX - spacing * strokeWidth);
+    rect.setAttributeNS(null, 'fill', 'none');
+    rect.setAttributeNS(null, 'stroke', 'black');
+    rect.setAttributeNS(null, 'stroke-width', strokeWidth);
+    rect.setAttributeNS(null, 'rx', rounding);
+    rect.setAttributeNS(null, 'pointer-events', 'all');
+    rect.setAttributeNS(null, 'class', 'rect');
+ 		rect.setAttributeNS(null, 'id', `rect_${i}${j}`);
+
+    
+    let text = document.createElementNS(svgns, 'text');
+    text.setAttributeNS(null, 'x', j * gridX + spacing * strokeWidth + (gridX - spacing * strokeWidth)/2);
+    text.setAttributeNS(null, 'y', i * gridY + spacing * strokeWidth + (gridY - spacing * strokeWidth)/1.5);
+    text.setAttributeNS(null, 'text-anchor', 'middle');
+    text.setAttributeNS(null, 'class', 'text');
+  	text.setAttributeNS(null, 'id', `text_${i}${j}`);
+    //text.setAttributeNS(null, 'textLength', gridX - spacing * strokeWidth);
+    //text.setAttributeNS(null, 'lengthAdjust', 'spacingAndGyphs');
+    text.innerHTML = currentNum;
+    
+    g.appendChild(rect);
+    g.appendChild(text);
+    
+    document.getElementById('grid').appendChild(g);
+    
+    currentNum++;
+  }
+}
+
+svg = document.getElementById('grid');
+
+clicked = (event) => {
+	let m = oMousePosSVG(event);
+  console.log(m.y, m.x);
+  updateSelectStatus(Math.floor(m.y / gridY), Math.floor(m.x / gridX));
+}
+
+var grid = [];
+
+var count = 1;
+
+for (let i = 0; i < rows; i++) {
+	grid.push([])
+	for (let j = 0; j < cols; j++) {
+  	grid[i].push({
+    	color: "",
+      selected: false,
+      drawn: false,
+      value: count,
+      neighbors: 0,
+      id: `${i}${j}`
+    });
+    if (grid[i][j].selected) {
+      document.getElementById(`${i}${j}`).classList.add("selected");
+    } else {
+      document.getElementById(`${i}${j}`).classList.remove("selected");
+      count++;
+  }
+  }
+}
+
+randomizeCells = () => {
+	for (let i = 0; i < rows; i++) {
+  	for (let j = 0 ; j < cols; j++) {
+    	grid[i][j].selected = ( Math.round(Math.random()) == 0 ? false : true );
+    }
+  }
+} 
+
+// const kenoNums = Array.from(Array(80), (_, i) => i + 1)
+const kenoNums = [...Array(80).keys()];
+
+shuffle = (arr) => {
+	for (let i = arr.length - 1; i > 0; i--) {
+  	randNum = Math.floor(Math.random() * i);
+    temp = arr[randNum];
+    arr[randNum] = arr[i];
+    arr[i] = temp;
+  }
+}
+
+shuffle(kenoNums);
+
+getCoords = (num) => {
+	return {
+  	i: Math.floor(num / 10),
+    j: (num % 10)
+  };
+}
+
+var playerNumbers = {};
+var drawnNumbers = [];
+
+pickNums = () => {
+	for (let i = 0; i < 20; i++) {
+  	pick = getCoords(kenoNums[i])
+    // console.log(kenoNums[i], pick)
+  	document.getElementById(`rect_${pick.i}${pick.j}`).classList.add('drawn');
+    grid[pick.i][pick.j].drawn = true;
+    drawnNumbers.push(kenoNums[i]);
+  }
+  document.getElementById('drawn').innerHTML = drawnNumbers;
+}
+
+clearDrawing = () => {
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < cols; j++) {
+     	if (grid[i][j].drawn == true) {
+      	document.getElementById(`rect_${i}${j}`).classList.remove('drawn');
+        grid[i][j].drawn = false;
+      }
+    }
+  }
+  drawnNumbers = [];
+  document.getElementById('drawn').innerHTML = "";
+
+}
+
+pickNums();
+
+const updateSelectStatus = (i, j) => {
+	
+  console.log(i, j);
+  grid[i][j].selected = !grid[i][j].selected; 
+  if (grid[i][j].selected) {
+  	document.getElementById(`rect_${i}${j}`).classList.add('selected');
+    playerNumbers[grid[i][j].value] = false;
+    document.getElementById('player-nums').innerHTML = Object.keys(playerNumbers);
+  } else {
+  	document.getElementById(`rect_${i}${j}`).classList.remove("selected");
+    delete playerNumbers[grid[i][j].value];
+    document.getElementById('player-nums').innerHTML = Object.keys(playerNumbers);
+  }
+}
+
+oMousePosSVG = (e) => {
+	let p = svg.createSVGPoint();
+  p.x = e.clientX;
+  p.y = e.clientY;
+  let ctm = svg.getScreenCTM().inverse();
+  p = p.matrixTransform(ctm);
+  return p;
+}
+
+svg.addEventListener("click", clicked);
+
